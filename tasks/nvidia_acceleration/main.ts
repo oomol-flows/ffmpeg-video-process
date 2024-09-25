6d326d77a5974960923cf326d4e53ba1f084f625
l1shen <648952316@qq.com>
2024-09-25 17:12:39 +0800

    Update main.ts

--- tasks/nvidia_acceleration/main.ts
+++ tasks/nvidia_acceleration/main.ts
@@ -10,12 +10,13 @@ export default async function (
 ): Promise<Outputs> {
   const newFF = inputs.ffmpeg_source
     .clone()
-    .outputOptions("-vsync 0")
-    .outputOptions("-hwaccel cuvid")
+    .inputOption([
+      "-vsync 0",
+      "-hwaccel cuvid",
+      "-c:v h264_cuvid"
+    ])
     .inputFormat("mp4")
-    .videoCodec("h264_cuvid") // 解码使用cuvid
     .audioCodec("copy") // 音频流直接复制
     .videoCodec("h264_nvenc") // 编码使用nvenc
-    .videoBitrate("5M"); // 视频比特率设置为5Mbps
   return { ffmpeg_source: newFF };
 }
